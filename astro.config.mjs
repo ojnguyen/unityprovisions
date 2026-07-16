62781de75d163bf52ebe050ab1016916b775df20
Owen Nguyen <owennguyen18@gmail.com>
2026-07-16 17:01:01 -0400

    Set up Astro project architecture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://unityprovisions.org",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap(), mdx()]
+});
